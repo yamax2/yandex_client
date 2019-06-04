bec16aebda12059fc18113a2ded0550dc29eee7c
Maxim Tretyakov <max@tretyakov-ma.ru>
2019-06-04 22:47:23 +0500

    dav client: put, delete, mkcol

--- lib/yandex_photo_storage/dav/client.rb
+++ lib/yandex_photo_storage/dav/client.rb
@@ -3,9 +3,30 @@ require 'digest'
 module YandexPhotoStorage
   module Dav
     # https://tech.yandex.ru/disk/doc/dg/reference/put-docpage/
+    #
+    # cli = YandexPhotoStorage::Dav::Client.new(access_token: access_token)
+    #
+    # cli.put(file: '1.txt', name: '/a/b/c/1.txt')
+    # cli.delete(name: '/a/b/c/1.txt')
+    # cli.mkcol(name: '/a/b/c')
     class Client < ::YandexPhotoStorage::Client
       ACTION_URL = 'https://webdav.yandex.ru'.freeze
-      ACTIONS = %i[put get mkcol delete].freeze
+
+      ACTIONS = {
+        put: lambda do |params|
+          filename = params.fetch(:file)
+
+          {
+            Etag: Digest::MD5.file(filename).to_s,
+            Sha256: Digest::SHA256.file(filename).to_s,
+            Expect: '100-continue',
+            'Content-Length' => File.size(filename).to_s
+          }
+        end,
+
+        mkcol: nil,
+        delete: nil
+      }.freeze
 
       def initialize(access_token:)
         @access_token = access_token
@@ -22,35 +43,20 @@ module YandexPhotoStorage
       end
 
       def request_body(action, params)
-        case action
-        when :put
-          File.read(params.fetch(:file))
-        else raise 'unreachable'
-        end
+        File.read(params.fetch(:file)) if action == :put
       end
 
       def request_headers(action, params)
-        case action
-        when :put
-          filename = params.fetch(:file)
+        proc = ACTIONS.fetch(action)
 
-          {
-            Authorization: "OAuth #{@access_token}",
-            Etag: Digest::MD5.file(filename).to_s,
-            Sha256: Digest::SHA256.file(filename).to_s,
-            Expect: '100-continue',
-            'Content-Length' => File.size(filename).to_s
-          }
-        else raise 'unreachable'
-        end
+        headers = {Authorization: "OAuth #{@access_token}"}
+        headers.merge!(proc.call(params)) if proc.present?
+
+        headers
       end
 
-      def request_uri(action, params)
-        case action
-        when :put
-          URI.parse("#{ACTION_URL}#{params.fetch(:upload_file_name)}")
-        else raise 'unreachable'
-        end
+      def request_uri(_action, params)
+        URI.parse("#{ACTION_URL}#{params.fetch(:name)}")
       end
     end
   end
