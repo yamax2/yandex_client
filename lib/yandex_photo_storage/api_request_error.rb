50574c6063d1c37ea934495ac714391eb68c0be7
Maxim Tretyakov <max@tretyakov-ma.ru>
2019-06-25 23:36:24 +0500

    fix: attr_accessor -> attr_reader in exception class

--- lib/yandex_photo_storage/api_request_error.rb
+++ lib/yandex_photo_storage/api_request_error.rb
@@ -1,11 +1,11 @@
 module YandexPhotoStorage
   class ApiRequestError < StandardError
-    attr_accessor :error, :error_description, :code
+    attr_reader :error, :error_description, :code
 
     def initialize(error:, error_description:, code:)
       @error = error
       @error_description = error_description
-      @code = code
+      @code = code.to_i
 
       super [error, error_description, "http code #{@code}"].compact.join(', ')
     end
